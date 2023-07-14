0bb32171d6cf7a08cc878fdd2243ab1e85f261a1
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-07-14 11:46:43 -0700

    initial sim function

---
+++ src/sim.jl
@@ -0,0 +1,4 @@
+function Sim(ego_ip::InteractionPlanner, other_ip::InteractionPlanner, sim_horizon::Int64)
+    # Given the IP problem setup of the ego agent and other agent
+    # Uses MPC function to simulate to a given time horizon
+end
