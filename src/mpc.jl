848845d3d423643c6da755bc7bfe29c74eba5f8a
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-07-14 11:40:14 -0700

    initial MPC planner

---
+++ src/mpc.jl
@@ -0,0 +1,6 @@
+function MPC_step(ip::InteractionPlanner)
+    # MPC Step function that takes inputs of current ego state and current state of other agent
+    # Returns optimal ego control 
+
+
+end
