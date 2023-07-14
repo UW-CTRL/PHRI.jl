ed7ad1aada2380e548c4583ea3aa8bd8294cde38
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-07-14 14:28:04 -0700

    sim update

--- src/sim.jl
+++ src/sim.jl
@@ -1,4 +1,9 @@
 function Sim(ego_ip::InteractionPlanner, other_ip::InteractionPlanner, sim_horizon::Int64)
     # Given the IP problem setup of the ego agent and other agent
+    ego_initial_control = ego_ip.ego_planner.incon.opt_params.previous_controls[1]
+    other_initial_control = other_ip.ego_planner.incon.opt_params.previous_controls[1]
+    for i in 1:sim_horizon
+        next_ego_state = step(ego_ip.ego_planner.)
+
     # Uses MPC function to simulate to a given time horizon
 end
