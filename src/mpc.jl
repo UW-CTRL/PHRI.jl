8a4cfa535a8e002fd58934f73a8fd05b21af947f
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-07-17 14:17:07 -0700

    Sim + MPC work when the model dynamics used for each agent are identical, needs to be updated to accomodate all dynamics

--- src/mpc.jl
+++ src/mpc.jl
@@ -1,7 +1,7 @@
-function MPC_step(ip::InteractionPlanner, ego_actual_state::Vector{T}, other_actual_state::Vector{T}; ibr_iterations=3::Int64, leader="ego"::String) where {T}
+function MPC_step(ip::InteractionPlanner, ego_actual_state::Vector{T}, other_actual_state::Vector{T}; ibr_iterations::Int64, leader::String) where {T}
     ip.ego_planner.ideal.opt_params.initial_state = ego_actual_state
     ip.other_planner.ideal.opt_params.initial_state = other_actual_state
-    _, _, ego_control = IteratedBestresponse(ip, ibr_iterations, leader)
+    _, _, ego_control = IteratedBestResponseMPC(ip, ibr_iterations, leader)
     # MPC Step function that takes inputs of current ego state and current state of other agent
     # Returns optimal ego control 
     return ego_control[1]
