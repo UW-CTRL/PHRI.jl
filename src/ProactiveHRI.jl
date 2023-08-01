c183baa3d7f7cf4700ab0d6b288b3bf6474ac74d
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-08-01 11:35:15 -0700

    added package dependencies

--- src/ProactiveHRI.jl
+++ src/ProactiveHRI.jl
@@ -15,6 +15,7 @@ export diagm
 export SingleIntegrator2D, DoubleIntegrator2D, SingleIntegratorPolar2D, Unicycle, DynamicallyExtendedUnicycle
 export PlannerHyperparameters, PlannerOptimizerParams, InconvenienceProblem, IdealProblem, update_problem!, solve, InteractionPlanner, ibr, ibr_save, ibr_mpc
 export mpc_step, Sim
+export plot_solve_solution, animation, avoidance_animation
 
 include("dynamics.jl")
 include("planner.jl")
