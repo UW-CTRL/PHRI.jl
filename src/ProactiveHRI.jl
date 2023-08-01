d160a8f7ed320d481e87f185dfa952228d65ec8c
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-08-01 11:24:57 -0700

    added package dependencies

--- src/ProactiveHRI.jl
+++ src/ProactiveHRI.jl
@@ -1,6 +1,14 @@
 module ProactiveHRI
 
 # Write your package code here.
+using LinearAlgebra
+using ForwardDiff
+using Parameters
+using JuMP, HiGHS, ECOS
+using EllipsisNotation
+using Parameters
+using AngleBetweenVectors
+
 
 export SingleIntegrator2D, DoubleIntegrator2D, SingleIntegratorPolar2D, Unicycle, DynamicallyExtendedUnicycle
 export PlannerHyperparameters, PlannerOptimizerParams, InconvenienceProblem, IdealProblem, update_problem!, solve, InteractionPlanner, ibr, ibr_save, ibr_mpc
