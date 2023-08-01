15528a9fa885e072b3863b20ada0ea2b10ba76ef
Jasper Geldenbott <jasper.geldenbott@gmail.com>
2023-08-01 11:17:04 -0700

    removed precompile

--- src/ProactiveHRI.jl
+++ src/ProactiveHRI.jl
@@ -4,8 +4,6 @@ module ProactiveHRI
 
 export SingleIntegrator2D, DoubleIntegrator2D, SingleIntegratorPolar2D, Unicycle, DynamicallyExtendedUnicycle
 
-export dynamics.jl
-
 include("dynamics.jl")
 include("planner.jl")
 include("planner_utils.jl")
@@ -13,7 +11,5 @@ include("utils.jl")
 include("plotting.jl")
 include("mpc.jl")
 include("sim.jl")
-
-import SingleIntegrator2D, DoubleIntegrator2D, SingleIntegratorPolar2D, Unicycle, DynamicallyExtendedUnicycle
 
 end
